9a2778c83663bcd4944505a122574bc5b54d7e71
Dylan Wright <dylan.wright@and.digital>
2023-10-19 10:07:32 +0100

    Adds encryption v2 service

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -2,12 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from '../user/user.service';
 import { User } from '../user/user.entity';
 import { UserController } from './user.controller';
+import { EncryptionServiceV2 } from '../encryption/encryptionV2.service';
 
 const mockDate = new Date('2022-03-25T14:00:00.000Z');
 jest.spyOn(global, 'Date').mockImplementation(() => mockDate);
 describe('UserController', () => {
     let controller: UserController;
     let userService: UserService;
+    let encryptionServiceV2: EncryptionServiceV2;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -25,11 +27,19 @@ describe('UserController', () => {
                         migrateOrCreate: jest.fn(),
                     },
                 },
+                {
+                    provide: EncryptionServiceV2,
+                    useValue: {
+                        decryptV2: jest.fn(),
+                    },
+                },
             ],
         }).compile();
 
         controller = module.get<UserController>(UserController);
         userService = module.get<UserService>(UserService);
+        encryptionServiceV2 =
+            module.get<EncryptionServiceV2>(EncryptionServiceV2);
     });
 
     it('should be defined', () => {
@@ -40,22 +50,23 @@ describe('UserController', () => {
         it('should get all the savedSearches based on the user id', async () => {
             const mockUser = new User();
             jest.spyOn(userService, 'migrateOrCreate').mockResolvedValue({
-                isExistingUser: true,
-                user: mockUser,
+                isNewUser: true,
             });
 
-            const response = await controller.migrate(
+            jest.spyOn(encryptionServiceV2, 'decryptV2').mockResolvedValue(
                 'john.doe@cabinetoffice.gov.uk',
-                '1234',
             );
+
+            const email = Buffer.from('john.doe@cabinetoffice.gov.uk', 'utf-8');
+
+            const response = await controller.migrate(email, '1234');
 
             expect(userService.migrateOrCreate).toHaveBeenCalledWith(
                 'john.doe@cabinetoffice.gov.uk',
                 '1234',
             );
             expect(response).toStrictEqual({
-                isExistingUser: true,
-                user: mockUser,
+                isNewUser: true,
             });
         });
     });
