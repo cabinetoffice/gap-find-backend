8f4d37db652e42caf6c7233b4fcf07d1676f692a
Dylan Wright <dylan.wright@and.digital>
2023-11-23 16:58:04 +0000

    fixes lint errors

--- src/migration/1700651901473-cascade-delete-user.ts
+++ src/migration/1700651901473-cascade-delete-user.ts
@@ -1,32 +1,71 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class cascadeDeleteUser1700651901473 implements MigrationInterface {
-    name = 'cascadeDeleteUser1700651901473'
+    name = 'cascadeDeleteUser1700651901473';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "saved_search" DROP CONSTRAINT "FK_397e5fcfff614ace7edf6831d5b"`);
-        await queryRunner.query(`ALTER TABLE "subscription" DROP CONSTRAINT "FK_cc906b4bc892b048f1b654d2aa0"`);
-        await queryRunner.query(`ALTER TABLE "saved_search_notification" DROP CONSTRAINT "FK_a9dd34a7c89d31227a4b4e2eb85"`);
-        await queryRunner.query(`ALTER TABLE "unsubscribe" DROP CONSTRAINT "FK_f051628403f51d0b0e11c1aabf4"`);
-        await queryRunner.query(`ALTER TABLE "newsletter" DROP CONSTRAINT "FK_20f63020913bbfdc1835e080549"`);
-        await queryRunner.query(`ALTER TABLE "saved_search" ADD CONSTRAINT "FK_397e5fcfff614ace7edf6831d5b" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "subscription" ADD CONSTRAINT "FK_cc906b4bc892b048f1b654d2aa0" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "saved_search_notification" ADD CONSTRAINT "FK_a9dd34a7c89d31227a4b4e2eb85" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "unsubscribe" ADD CONSTRAINT "FK_f051628403f51d0b0e11c1aabf4" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "newsletter" ADD CONSTRAINT "FK_20f63020913bbfdc1835e080549" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(
+            `ALTER TABLE "saved_search" DROP CONSTRAINT "FK_397e5fcfff614ace7edf6831d5b"`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "subscription" DROP CONSTRAINT "FK_cc906b4bc892b048f1b654d2aa0"`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "saved_search_notification" DROP CONSTRAINT "FK_a9dd34a7c89d31227a4b4e2eb85"`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "unsubscribe" DROP CONSTRAINT "FK_f051628403f51d0b0e11c1aabf4"`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "newsletter" DROP CONSTRAINT "FK_20f63020913bbfdc1835e080549"`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "saved_search" ADD CONSTRAINT "FK_397e5fcfff614ace7edf6831d5b" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "subscription" ADD CONSTRAINT "FK_cc906b4bc892b048f1b654d2aa0" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "saved_search_notification" ADD CONSTRAINT "FK_a9dd34a7c89d31227a4b4e2eb85" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "unsubscribe" ADD CONSTRAINT "FK_f051628403f51d0b0e11c1aabf4" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "newsletter" ADD CONSTRAINT "FK_20f63020913bbfdc1835e080549" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "newsletter" DROP CONSTRAINT "FK_20f63020913bbfdc1835e080549"`);
-        await queryRunner.query(`ALTER TABLE "unsubscribe" DROP CONSTRAINT "FK_f051628403f51d0b0e11c1aabf4"`);
-        await queryRunner.query(`ALTER TABLE "saved_search_notification" DROP CONSTRAINT "FK_a9dd34a7c89d31227a4b4e2eb85"`);
-        await queryRunner.query(`ALTER TABLE "subscription" DROP CONSTRAINT "FK_cc906b4bc892b048f1b654d2aa0"`);
-        await queryRunner.query(`ALTER TABLE "saved_search" DROP CONSTRAINT "FK_397e5fcfff614ace7edf6831d5b"`);
-        await queryRunner.query(`ALTER TABLE "newsletter" ADD CONSTRAINT "FK_20f63020913bbfdc1835e080549" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "unsubscribe" ADD CONSTRAINT "FK_f051628403f51d0b0e11c1aabf4" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "saved_search_notification" ADD CONSTRAINT "FK_a9dd34a7c89d31227a4b4e2eb85" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "subscription" ADD CONSTRAINT "FK_cc906b4bc892b048f1b654d2aa0" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "saved_search" ADD CONSTRAINT "FK_397e5fcfff614ace7edf6831d5b" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(
+            `ALTER TABLE "newsletter" DROP CONSTRAINT "FK_20f63020913bbfdc1835e080549"`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "unsubscribe" DROP CONSTRAINT "FK_f051628403f51d0b0e11c1aabf4"`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "saved_search_notification" DROP CONSTRAINT "FK_a9dd34a7c89d31227a4b4e2eb85"`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "subscription" DROP CONSTRAINT "FK_cc906b4bc892b048f1b654d2aa0"`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "saved_search" DROP CONSTRAINT "FK_397e5fcfff614ace7edf6831d5b"`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "newsletter" ADD CONSTRAINT "FK_20f63020913bbfdc1835e080549" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "unsubscribe" ADD CONSTRAINT "FK_f051628403f51d0b0e11c1aabf4" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "saved_search_notification" ADD CONSTRAINT "FK_a9dd34a7c89d31227a4b4e2eb85" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "subscription" ADD CONSTRAINT "FK_cc906b4bc892b048f1b654d2aa0" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "saved_search" ADD CONSTRAINT "FK_397e5fcfff614ace7edf6831d5b" FOREIGN KEY ("userId") REFERENCES "gap_user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+        );
     }
-
 }
