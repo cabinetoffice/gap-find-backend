37d3ec5a43b96676aa64ac5bc214be73fb099c98
JamieGunnCO <jamie.gunn@cabinetoffice.gov.uk>
2023-11-28 15:53:38 +0000

    fail on startup when .env config missing

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { config } from 'dotenv-safe';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const checkEnvConfigPresent = async () => {
+    try {
+        await config();
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+};
+
 async function bootstrap() {
+    await checkEnvConfigPresent();
     const app = await NestFactory.create(AppModule);
     await app.listen(process.env.PORT || 3001);
 }
+
 bootstrap();
