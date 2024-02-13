b50da22f36ba6724fb1db2c23b6c237560a07cdc
john-tco <john.morley@cabinetoffice.gov.uk>
2024-02-13 11:38:48 +0000

    undo rm

--- src/scheduler/scheduler.providers.ts
+++ src/scheduler/scheduler.providers.ts
@@ -1,7 +1,8 @@
 import { SchedulerRegistry } from '@nestjs/schedule';
+import { CronJob } from 'cron';
 import { Connection } from 'typeorm';
 import { NotificationsService } from '../notifications/notifications.service';
-import { ScheduledJob } from './scheduled-job.entity';
+import { ScheduledJob, ScheduledJobType } from './scheduled-job.entity';
 import { v2NotificationsService } from 'src/notifications/v2/notifications.service';
 
 export const SchedulerProviders = [
@@ -9,18 +10,88 @@ export const SchedulerProviders = [
         provide: 'Scheduler',
         useFactory: async (
             connection: Connection,
+            notificationService: NotificationsService,
             v2NotificationsService: v2NotificationsService,
             schedulerRegistry: SchedulerRegistry,
         ) => {
             const scheduledJobRepo = connection.getRepository(ScheduledJob);
 
             const jobs = await scheduledJobRepo.find();
+
             console.log(schedulerRegistry.getCronJobs());
             for (const [index, job] of jobs.entries()) {
-                v2NotificationsService.processScheduledJob(job, index);
-                continue;
+                if (process.env['FIND_ACCOUNTS_MIGRATION_ENABLED']) {
+                    v2NotificationsService.processScheduledJob(job, index);
+                } else {
+                    switch (job.type) {
+                        case ScheduledJobType.GRANT_UPDATED:
+                            const grantUpdatedJob = new CronJob(
+                                job.timer,
+                                () => {
+                                    notificationService.processGrantUpdatedNotifications();
+                                },
+                            );
+                            schedulerRegistry.addCronJob(
+                                `GRANT_UPDATED_${index}`,
+                                grantUpdatedJob,
+                            );
+                            grantUpdatedJob.start();
+                            break;
+                        case ScheduledJobType.GRANT_UPCOMING:
+                            const grantUpcoming = new CronJob(job.timer, () => {
+                                notificationService.processGrantUpcomingNotifications();
+                            });
+                            schedulerRegistry.addCronJob(
+                                `GRANT_UPCOMING${index}`,
+                                grantUpcoming,
+                            );
+                            grantUpcoming.start();
+                            break;
+                        case ScheduledJobType.NEW_GRANTS:
+                            const newGrants = new CronJob(job.timer, () => {
+                                notificationService.processNewGrantsNotifications();
+                            });
+                            schedulerRegistry.addCronJob(
+                                `NEW_GRANT${index}`,
+                                newGrants,
+                            );
+                            newGrants.start();
+                            break;
+                        case ScheduledJobType.SAVED_SEARCH_MATCHES:
+                            const savedSearchMatches = new CronJob(
+                                job.timer,
+                                () => {
+                                    notificationService.processSavedSearchMatches();
+                                },
+                            );
+                            schedulerRegistry.addCronJob(
+                                `SAVED_SEARCH_MATCHES${index}`,
+                                savedSearchMatches,
+                            );
+                            savedSearchMatches.start();
+                            break;
+                        case ScheduledJobType.SAVED_SEARCH_MATCHES_NOTIFICATION:
+                            const savedSearchMatchesNotification = new CronJob(
+                                job.timer,
+                                () => {
+                                    notificationService.processSavedSearchMatchesNotifications();
+                                },
+                            );
+                            schedulerRegistry.addCronJob(
+                                `SAVED_SEARCH_MATCHES_NOTIFICATION_${index}`,
+                                savedSearchMatchesNotification,
+                            );
+                            savedSearchMatchesNotification.start();
+                            break;
+                    }
+                }
             }
         },
-        inject: [Connection, v2NotificationsService, SchedulerRegistry],
+        inject: [
+            Connection,
+            NotificationsService,
+            v2NotificationsService,
+            SchedulerRegistry,
+        ],
     },
 ];
