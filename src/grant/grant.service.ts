8a84f91f961d0c8a20209e5c1dfe9be05556d6f7
arul-foureals <arul@fourseals.co.uk>
2024-12-09 23:01:29 +0000

    setGrantUpdated set to false added

--- src/grant/grant.service.ts
+++ src/grant/grant.service.ts
@@ -32,6 +32,22 @@ export class GrantService {
         return ids;
     }
 
+    async setGrantUpdatedToFalse(grantIds:string[]) {
+        console.log("Updating elastic index for grantUpdated to false");
+        console.log('Updating elasticsearch - total:'+grantIds.length);
+        for (const grantId of grantIds) {
+            console.log('Grant ID :'+grantId);
+            this.elasticsearchService.update({
+                index: this.config.get('ELASTIC_INDEX'),
+                id: grantId,
+                body: {
+                    'fields.grantUpdated.en-US': false,
+                }
+            });
+            console.log('Grant ID :'+grantId+'   updated to false..');
+        }
+    }
+    
     async findAllUpcomingOpeningGrants(): Promise<ContentfulGrant[]> {
         const startOfDayInTwoWeeksTime = this.getStartOfDayInXDays(14);
         const endOfDayInTwoWeeksTime = this.getEndOfDayInXDays(14);
